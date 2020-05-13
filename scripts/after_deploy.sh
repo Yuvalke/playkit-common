b80040f1a840c987b7b629adb0573e6136075441
Yuval keidar <yuval.keidar@kaltura.com>
2020-05-13 09:16:54 +0300

    fix(yuval-test): check check check

--- scripts/after_deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -k -d "{'name':$1, 'version':$2, 'source':'npm'}" -H "Content-Type: application/json" -X POST https://jenkins.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$3
