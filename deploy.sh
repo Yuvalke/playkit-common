6d673d90aef6a212f16c0f20f00293b6447ddf89
yuval.keidar <keidaryu@gmail.com>
2020-12-29 14:02:43 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,12 @@ echo $1
 echo $2
 echo $3
 REAL_BRANCH=$(git ls-remote origin | sed -n "\|$TRAVIS_COMMIT\s\+refs/heads/|{s///p}")
-echo $REAL_BRANCH
+echo "$REAL_BRANCH"
+UPDATE_UI_CONF=true
+if [ "$REAL_BRANCH" = "master" ]; then
+  UPDATE_UI_CONF=true
+else
+  UPDATE_UI_CONF=false
+fi
+
+echo $UPDATE_UI_CONF
