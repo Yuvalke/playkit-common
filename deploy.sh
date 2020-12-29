a5d419768fda4a6afe83aee90024be4707dc4b0d
yuval.keidar <keidaryu@gmail.com>
2020-12-29 16:55:21 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,10 @@ echo $3
 echo $TRAVIS_COMMIT
 BRANCH=$(git name-rev --name-only --exclude="tags/*" "$TRAVIS_COMMIT")
 REAL_BRANCH=$(git ls-remote origin | sed -n "\|$TRAVIS_COMMIT\s\+refs/heads/|{s///p}")
+IS_RELEASE=$(git ls-remote origin | grep "$TRAVIS_COMMIT\s\+refs/heads/release$")
+
 echo "$BRANCH"
+echo "$IS_RELEASE"
 echo "$REAL_BRANCH"
 UPDATE_UI_CONF=true
 if [ "$REAL_BRANCH" = "master" ]; then
