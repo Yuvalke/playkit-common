a5199e7f9feea5ea140428b90d68b55236557c6f
yuval.keidar <keidaryu@gmail.com>
2020-12-29 19:13:13 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,9 @@ echo $1
 echo $2
 echo $3
 echo $TRAVIS_COMMIT
-BRANCH=$(git name-rev --name-only --exclude="tags/*" "$TRAVIS_COMMIT")
 REAL_BRANCH=$(git ls-remote origin | sed -n "\|$TRAVIS_COMMIT\s\+refs/heads/|{s///p}")
 IS_RELEASE=$(git ls-remote origin | grep "$TRAVIS_COMMIT\s\+refs/heads/release$")
 
-echo "$BRANCH"
 echo "$IS_RELEASE"
 echo "$REAL_BRANCH"
 UPDATE_UI_CONF=true
