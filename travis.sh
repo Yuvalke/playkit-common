59c52948a99e29358763f39c05253cb17febd927
yuval.keidar <keidaryu@gmail.com>
2021-06-04 05:13:56 +0300

    add test

--- travis.sh
+++ travis.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [[ "$TRAVIS_BRANCH" = "master" ]] && [[ "$TRAVIS_EVENT_TYPE" != "pull_request" ]] && [[ ! "$TRAVIS_COMMIT_MESSAGE" =~ ^(chore).*(update dist)$ ]] && [[ ! "$TRAVIS_COMMIT_MESSAGE" =~ ^chore\(release\) ]]; then
+  echo "entered!!!!!"
+fi
+
 echo "========> ${TRAVIS_MODE} !!!!"
 echo "========> ${yuval} !!!!"
