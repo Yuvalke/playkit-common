1d0a59411f3301aaa1fb927ec4da81a87106844c
yuval.keidar <keidaryu@gmail.com>
2021-06-10 09:13:31 +0300

    sdfds

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-echo "=======> deploy! ${TRAVIS_MODE}"
-echo "$TRAVIS_COMMIT"
-REAL_BRANCH=$(git ls-remote origin | sed -n "\|$TRAVIS_COMMIT\s\+refs/heads/|{s///p}")
+git_hash=$(git rev-parse --short "$GITHUB_SHA")
+echo "$GITHUB_SHA"
+echo "$git_hash"
+REAL_BRANCH=$(git ls-remote origin | sed -n "\|$git_hash\s\+refs/heads/|{s///p}")
+REAL_BRANCH1=$(git ls-remote origin | sed -n "\|$GITHUB_SHA\s\+refs/heads/|{s///p}")
 echo "REAL_BRANCH $REAL_BRANCH"
+echo "REAL_BRANCH1 $REAL_BRANCH1"
 if [ "$REAL_BRANCH" = "master" ]; then
   echo "master!!!!!!"
 else
